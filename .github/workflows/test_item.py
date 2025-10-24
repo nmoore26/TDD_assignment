5538df3e585c2b72b93f84c56ec9545b3d2e75bc
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-24 13:22:56 -0500

    Remove tests for Item class in test_item.py

--- .github/workflows/test_item.py
+++ .github/workflows/test_item.py
@@ -1,10 +1,2 @@
-from item.py import Item
 
-def test_item():
-  orange = Item("orange", 0.50, 20)
-  assert str(orange) == "orange $0.50 20"
-
-def test_display():
-  strawberry = Item("strawberry", 0.70, 35)
-  assert strawberry.display() == "strawberry"
 
