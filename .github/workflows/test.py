97ccfff6c918f99a824f38bde26c7e1d9c908e5c
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-24 12:39:22 -0500

    Fix string assertion in test_item function

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,6 +1,6 @@
 def test_item():
   orange = Item("orange", 0.50, 20)
-  assertEquals(item.str(),"orange $0.50 $20)
+  assertEquals(item.str(),"orange $0.50 $20")
 def test_display():
   strawberry = Item("strawberry", 1.0, 25)
   milk = Item("milk", 3.25, 15)
