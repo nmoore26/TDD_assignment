4b70de89d59b5fd324933c027742fe2dc04853da
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-24 13:09:21 -0500

    Rename test_display_name to test_display

--- .github/workflows/test_p.py
+++ .github/workflows/test_p.py
@@ -4,7 +4,7 @@ def test_item():
   orange = Item("orange", 0.50, 20)
   assert str(orange) == "orange $0.50 20"
 
-def test_display_name():
+def test_display():
   strawberry = Item("strawberry", 0.70, 35)
   assert strawberry.display() == "strawberry"
 
