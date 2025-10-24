ceb95f3919989a2d65ee52d0230662a5fc3326e5
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-24 13:05:21 -0500

    Refactor Item class methods and formatting

--- item.py
+++ item.py
@@ -1,10 +1,12 @@
 class Item:
-  def __init__(self,name,price,count):
-    self.name = name
-    self.price = price
-    self.count = count
-  def __str__(self):
-    return f"{self.name} ${self.price} {self.count}"
-  
-  def display_name():
-    return self.name
+    def __init__(self, name, price, count):
+        self.name = name
+        self.price = price
+        self.count = count
+
+    def __str__(self):
+        return f"{self.name} ${self.price} {self.count}"
+
+    def display(self):
+        return self.name
+
