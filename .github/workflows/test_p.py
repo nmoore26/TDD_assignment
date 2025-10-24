307c5924d40c92eb5ffa9608ccb78633936d774c
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-24 13:06:38 -0500

    Fix typo in test function name

--- .github/workflows/test_p.py
+++ .github/workflows/test_p.py
@@ -4,7 +4,7 @@ def test_item():
   orange = Item("orange", 0.50, 20)
   assert orange.__str__() == "orange $0.50 20"
 
-def tes_display_name():
+def test_display_name():
   strawberry = Item("strawberry", 0.70, 35)
   assert strawberry.display() == "strawberry"
 
