d7344888f0b59af643623a06ac39a5b8837f4eed
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-24 13:03:11 -0500

    Add test for display_name method in test_p.py

--- .github/workflows/test_p.py
+++ .github/workflows/test_p.py
@@ -3,3 +3,8 @@ from item import *
 def test_item():
   orange = Item("orange", 0.50, 20)
   assert orange.__str__() == "orange $0.50 20"
+
+def tes_display_name():
+  strawberry = Item("strawberry", 0.70, 35)
+  assert strawberry.display_name() == "strawberry"
+
