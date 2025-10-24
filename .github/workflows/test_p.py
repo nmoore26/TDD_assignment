b99b04c4e5e7551802cab3bd054699eb8bf3366f
Nina Moore <135671726+nmoore26@users.noreply.github.com>
2025-10-24 13:08:00 -0500

    Fix string representation method in test_item

--- .github/workflows/test_p.py
+++ .github/workflows/test_p.py
@@ -2,7 +2,7 @@ from item import *
 
 def test_item():
   orange = Item("orange", 0.50, 20)
-  assert orange.__str__() == "orange $0.50 20"
+  assert str(orange) == "orange $0.50 20"
 
 def test_display_name():
   strawberry = Item("strawberry", 0.70, 35)
